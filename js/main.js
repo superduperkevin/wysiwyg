6b56d24fe9c1bdcccf28e5d9da0d4e2ac4ad7796
Kevin Amarbayar <KAmarbayar@talentpath.com>
2019-02-19 09:57:05 -0800

    modular toggle

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
+import toggleActive from './toggleActive.js';
+
 // select edit buttons
 const editButtons = document.querySelectorAll('.edit-button');
-console.log(editButtons);
 // add event listener (click)
 editButtons.forEach(button => button.addEventListener('click', toggleActive));
-
-// function that add a class to editable-text
-function toggleActive (event) {
-    // get the parent element
-    const  editableText = event.target.parentElement;
-    // toggle the class active (on some element)
-    editableText.classList.toggle('active');
-}
