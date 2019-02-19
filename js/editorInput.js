48719498e1b6bfa7c907249169da01e5d79f1f3e
Kevin Amarbayar <KAmarbayar@talentpath.com>
2019-02-19 12:04:42 -0800

    feature #1

--- js/editorInput.js
+++ js/editorInput.js
@@ -1,11 +1,16 @@
 // targets only the text area that changes the title
 
 export default function handleEditorInput(event) {
-    if (event.target.type === 'text' || 
-    event.target.tagName === 'TEXTAREA') {
-
+    console.log(event);
     const textElement = event.currentTarget.parentNode.querySelector('.text-element');
-    const text = event.target.value;
-    textElement.innerText = text;
-    }
+    if (event.target.type === 'text' || 
+        event.target.tagName === 'TEXTAREA') {
+        const text = event.target.value;
+        textElement.innerText = text;
+    } else {
+         // handle the styles
+        const { value, name: styleName} = event.target;
+        const unit = event.target.dataset.unit || '';
+        textElement.style[styleName] = `${value}${unit}`;
+        }
 }
